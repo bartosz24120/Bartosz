1e80c65cee632239a55bf59eca6eda67a8117cab
bartosz24120 <136969480+bartosz24120@users.noreply.github.com>
2023-06-24 13:25:24 +0200

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,10 @@
+def oblicz_srednia(liczby):
+    suma = sum(liczby)
+    srednia = suma / len(liczby)
+    return srednia
+
+# Przykładowe dane wejściowe
+liczby = [2, 4, 6, 8, 10]
+
+srednia = oblicz_srednia(liczby)
+print("Średnia wynosi:", srednia)
