1b88f80cab6b7fc16b4fbad608acf2cc454ed88b
bartosz24120 <136969480+bartosz24120@users.noreply.github.com>
2023-06-24 13:26:08 +0200

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,10 @@
+# Prosty program w Pythonie - powitanie użytkownika
+
+# Wypisanie powitania na konsolę
+print("Witaj w programie!")
+
+# Prośba o podanie imienia
+imie = input("Jak masz na imię? ")
+
+# Wyświetlenie komunikatu powitalnego z podanym imieniem
+print("Miło Cię poznać, " + imie + "!")
